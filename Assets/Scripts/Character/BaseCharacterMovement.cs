ec59808b5bf1456561b4badc5014037fd4b1a658
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2021-08-01 14:31:43 +0300

    change groundCheck

--- Assets/Scripts/Character/BaseCharacterMovement.cs
+++ Assets/Scripts/Character/BaseCharacterMovement.cs
@@ -11,21 +11,25 @@ public abstract class BaseCharacterMovement : NetworkBehaviour {
     [Header("Movement params")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private LayerMask layerMask;
 
     private float moveSpeedMultiplier = 1;
     private float jumpSpeedMultiplier = 1;
 
-    private Rigidbody2D rb;
+    private bool wasGrounded;
+
+    private Rigidbody2D myRb;
+    private Collider2D myCollider;
     private Animator animator;
 
     private void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        myRb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        myCollider = GetComponent<BoxCollider2D>();
     }
 
     public bool JumpInput { private get; set; }
     private int jumpCounter;
-    private bool isGrounded;
 
     public float MoveInput { private get; set; }
     private bool isMoveDisabled;
@@ -42,12 +46,7 @@ public abstract class BaseCharacterMovement : NetworkBehaviour {
         if (!hasAuthority) { return; }
 
         if (other.collider.CompareTag("Floor")) {
-            if (updateGroundRoutine != null) {
-                StopCoroutine(updateGroundRoutine);
-            }
-
             jumpCounter = 0;
-            isGrounded = true;
             animator.SetTrigger("Land");
         }
     }
@@ -57,8 +56,14 @@ public abstract class BaseCharacterMovement : NetworkBehaviour {
         if (!hasAuthority) { return; }
 
         if (other.collider.CompareTag("Floor")) {
-            updateGroundRoutine = StartCoroutine(nameof(UpdateGroundedFlagRoutine));
+            StartCoroutine(nameof(UpdateGroundedFlagRoutine));
         }
+    }
+
+    private IEnumerator UpdateGroundedFlagRoutine() {
+        wasGrounded = true;
+        yield return new WaitForSeconds(.1f);
+        wasGrounded = false;
     }
 
     [ClientCallback]
@@ -68,19 +73,14 @@ public abstract class BaseCharacterMovement : NetworkBehaviour {
         Move();
         Jump();
         Flip();
-        animator.SetFloat("HorizontalSpeed", Mathf.Abs(rb.velocity.x));
-        animator.SetFloat("VerticalSpeed", rb.velocity.y);
-    }
-
-    private IEnumerator UpdateGroundedFlagRoutine() {
-        yield return new WaitForSeconds(.1f);
-        isGrounded = false;
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(myRb.velocity.x));
+        animator.SetFloat("VerticalSpeed", myRb.velocity.y);
     }
 
     private void Move() {
         if (isMoveDisabled) { return; }
 
-        rb.velocity = new Vector2(MoveInput * moveSpeed * moveSpeedMultiplier * Time.fixedDeltaTime, rb.velocity.y);
+        myRb.velocity = new Vector2(MoveInput * moveSpeed * moveSpeedMultiplier * Time.fixedDeltaTime, myRb.velocity.y);
     }
 
     private void Jump() {
@@ -88,21 +88,39 @@ public abstract class BaseCharacterMovement : NetworkBehaviour {
 
         JumpInput = false;
 
-        Vector2 velocity = rb.velocity;
-        if (isGrounded) {
-            rb.velocity = new Vector2(velocity.x, jumpSpeed * jumpSpeedMultiplier);
+        Vector2 velocity = myRb.velocity;
+        if (IsGrounded() || wasGrounded) {
+            myRb.velocity = new Vector2(velocity.x, jumpSpeed * jumpSpeedMultiplier);
             jumpCounter = 1;
             animator.SetTrigger("Jump");
         } else if (jumpCounter == 1) {
-            rb.velocity = new Vector2(velocity.x, jumpSpeed * jumpSpeedMultiplier);
+            myRb.velocity = new Vector2(velocity.x, jumpSpeed * jumpSpeedMultiplier);
             animator.SetTrigger("Jump");
             jumpCounter = 2;
         }
     }
 
+    private bool IsGrounded() {
+        Debug.Log("IsGrounds Check");
+        float extra = .1f;
+        var bounds = myCollider.bounds;
+        RaycastHit2D hit = Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, bounds.extents.y + extra, layerMask);
+        Color color;
+        // if (hit.collider == null) {
+        //     color = Color.red;
+        // } else {
+        //     color = Color.green;
+        // }
+        // Debug.DrawRay(bounds.center + new Vector3(bounds.extents.x, 0), Vector2.down * (bounds.extents.y + extra), color);
+        // Debug.DrawRay(bounds.center - new Vector3(bounds.extents.x, 0), Vector2.down * (bounds.extents.y + extra), color);
+        // Debug.DrawRay(bounds.center - new Vector3(bounds.extents.x, bounds.extents.y + extra), Vector2.right * bounds.extents.x, color);
+        // Debug.Log(hit.collider == null);
+        return hit.collider != null;
+    }
+
     //TODO mb flip only on authoritive instance? will it sync?
     private void Flip() {
-        float velocityX = rb.velocity.x;
+        float velocityX = myRb.velocity.x;
         if (velocityX < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
         }
